fc007a859d48e9e7296a0edb00597166ef932b48
Chris Shaw <chris@christopher-shaw.com>
2020-09-20 17:34:47 -0400

    Completed code and tests

---
+++ tests/overrides.ts
@@ -0,0 +1,40 @@
+export const baseOverrides = {
+   album: "al\"bum",
+   artist: "ar\"tist",
+   albumArtist: "alb\"umArtist",
+   grouping: "grouping",
+   composer: "com\"poser",
+   year: 2019,
+   trackNumber: 2,
+   comment: "com\"ment",
+   genre: "ge\"nre",
+   copyright: "copy\"right",
+   description: "desc\"ription",
+   title: "tit\"le",
+   synopsis: "syno\"psis",
+}
+
+export const multiLineOverrides = {
+   album: "al\nbu\nm",
+   artist: "ar\ntis\nt",
+   albumArtist: "alb\numArtis\nt",
+   grouping: "gro\nupin\ng",
+   composer: "com\npose\nr",
+   year: 2019,
+   trackNumber: 2,
+   comment: "com\nmen\nt",
+   genre: "ge\nnr\ne",
+   copyright: "copy\nrigh\nt",
+   description: "desc\nriptio\nn",
+   synopsis: "syno\npsi\ns",
+   title: "tit\nl\ne",
+}
+
+export const singleOverride = {
+   composer: "composer",
+}
+
+export const pictureOverride = {
+   coverPicturePath: "tests\\test.jpg",
+   composer: "composer",
+}
