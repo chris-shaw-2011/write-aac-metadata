a0e83d6a7a2fe2230a27a671142c75af7c4f67ea
Chris Shaw <chris@christopher-shaw.com>
2024-09-04 21:21:56 -0400

    Fix some formatting

--- tests/overrides.ts
+++ tests/overrides.ts
@@ -1,4 +1,4 @@
-import path, {dirname} from "path"
+import path, { dirname } from "path"
 import { fileURLToPath } from "url"
 
 const __filename = fileURLToPath(import.meta.url)
@@ -6,42 +6,42 @@ const __dirname = dirname(__filename)
 
 
 export const baseOverrides = {
-   album: "\\a\\l\"b\"u'm'",
-   artist: "\\a\\r\"t\"is't'",
-   albumArtist: "\\a\\lb\"u\"mArtis't'",
-   grouping: "\\g\\r\"o\"upin'g'",
-   composer: "\\c\\om\"po\"se'r'",
-   year: 2019,
-   trackNumber: 2,
-   comment: "\\c\\om\"m\"en't'",
-   genre: "\\g\\e\"n\"r'e'",
-   copyright: "\\c\\opy\"r\"igh't'",
-   description: "\\d\\esc\"r\"iptio'n'",
-   title: "\\t\\i\"t\"l'e'",
-   synopsis: "\\s\\yn\"o\"psi's'",
+	album: "\\a\\l\"b\"u'm'",
+	artist: "\\a\\r\"t\"is't'",
+	albumArtist: "\\a\\lb\"u\"mArtis't'",
+	grouping: "\\g\\r\"o\"upin'g'",
+	composer: "\\c\\om\"po\"se'r'",
+	year: 2019,
+	trackNumber: 2,
+	comment: "\\c\\om\"m\"en't'",
+	genre: "\\g\\e\"n\"r'e'",
+	copyright: "\\c\\opy\"r\"igh't'",
+	description: "\\d\\esc\"r\"iptio'n'",
+	title: "\\t\\i\"t\"l'e'",
+	synopsis: "\\s\\yn\"o\"psi's'",
 }
 
 export const multiLineOverrides = {
-   album: "al\nbu\nm",
-   artist: "ar\ntis\nt",
-   albumArtist: "alb\numArtis\nt",
-   grouping: "gro\nupin\ng",
-   composer: "com\npose\nr",
-   year: 2019,
-   trackNumber: 2,
-   comment: "com\nmen\nt",
-   genre: "ge\nnr\ne",
-   copyright: "copy\nrigh\nt",
-   description: "desc\nriptio\nn",
-   synopsis: "syno\npsi\ns",
-   title: "tit\nl\ne",
+	album: "al\nbu\nm",
+	artist: "ar\ntis\nt",
+	albumArtist: "alb\numArtis\nt",
+	grouping: "gro\nupin\ng",
+	composer: "com\npose\nr",
+	year: 2019,
+	trackNumber: 2,
+	comment: "com\nmen\nt",
+	genre: "ge\nnr\ne",
+	copyright: "copy\nrigh\nt",
+	description: "desc\nriptio\nn",
+	synopsis: "syno\npsi\ns",
+	title: "tit\nl\ne",
 }
 
 export const singleOverride = {
-   composer: "composer",
+	composer: "composer",
 }
 
 export const pictureOverride = {
-   coverPicturePath: path.join(__dirname, "test.jpg"),
-   composer: "composer",
+	coverPicturePath: path.join(__dirname, "test.jpg"),
+	composer: "composer",
 }
